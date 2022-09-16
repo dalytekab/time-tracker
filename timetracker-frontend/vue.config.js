e835ebff848b189c6c371e0a4f67f8909263662e
Mohamed Ali Hachicha <mohamed.ali.hachicha@tekab.dev>
2022-09-16 11:01:30 +0000

    separated front-back

--- timetracker-frontend/vue.config.js
+++ timetracker-frontend/vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    allowedHosts: 'all',
+    client: {
+      webSocketURL: {
+        port:  443 ,
+      },
+    },
+  },
   transpileDependencies: true,
 
 })
